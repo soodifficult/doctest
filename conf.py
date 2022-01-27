c379172c20bf2245bb053847ea50842aa92c9afe
soodifficult <32888185+soodifficult@users.noreply.github.com>
2022-01-27 11:07:40 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -39,7 +39,7 @@ release = u'0.0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx_markdown_tables', 'myst_parser',
+    'myst_parser',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
